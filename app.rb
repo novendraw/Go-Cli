598486df2aa2d16f521c255d435c13baabfd0ee6
novendraw <ekanovendra726@gmail.com>
2018-06-20 23:21:21 +0700

    20x20 map

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 puts "Welcome"
 unit_cost = 300
 
-peta1 = Array[0,0,0,0,0]
-peta2 = Array[0,0,0,0,0]
-peta3 = Array[0,0,0,0,0]
-peta4 = Array[0,0,0,0,0]
-peta5 = Array[0,0,0,0,0]
+peta1 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta2 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta3 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta4 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta5 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta6 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta7 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta8 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta9 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta10 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta11 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta12 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta13 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta14 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta15 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta16 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta17 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta18 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta19 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+peta20 	= Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 
 print peta1
 puts ""
@@ -16,4 +32,34 @@ puts ""
 print peta4
 puts ""
 print peta5
+puts ""
+print peta6
+puts ""
+print peta7
+puts ""
+print peta8
+puts ""
+print peta9
+puts ""
+print peta10
+puts ""
+print peta11
+puts ""
+print peta12
+puts ""
+print peta13
+puts ""
+print peta14
+puts ""
+print peta15
+puts ""
+print peta16
+puts ""
+print peta17
+puts ""
+print peta18
+puts ""
+print peta19
+puts ""
+print peta20
 puts ""
