ab30ba5dfe719a29f62cf128255718242bd6c894
novendraw <ekanovendra726@gmail.com>
2018-06-20 23:15:28 +0700

    percobaan map

--- app.rb
+++ app.rb
@@ -1,1 +1,19 @@
 puts "Welcome"
+unit_cost = 300
+
+peta1 = Array[0,0,0,0,0]
+peta2 = Array[0,0,0,0,0]
+peta3 = Array[0,0,0,0,0]
+peta4 = Array[0,0,0,0,0]
+peta5 = Array[0,0,0,0,0]
+
+print peta1
+puts ""
+print peta2
+puts ""
+print peta3
+puts ""
+print peta4
+puts ""
+print peta5
+puts ""
